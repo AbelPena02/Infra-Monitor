aeb8eb9a3ed2b04fff19c5278c1311089331ecd2
AbelPena02 <penafigueroaabel@gmail.com>
2025-09-14 18:19:08 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", 
+        "postgresql://monitor:monitor123@localhost:5432/infra_monitor"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
