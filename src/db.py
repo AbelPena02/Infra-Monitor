6a2c0823a012ef17d8353ca93e7d6bd88c39da01
AbelPena02 <penafigueroaabel@gmail.com>
2025-09-14 18:51:47 -0700

    Update db.py

--- src/db.py
+++ src/db.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Server(db.Model):
-    __tablename__ = 'servers'
     id = db.Column(db.Integer, primary_key=True)
-    hostname = db.Column(db.String(100), nullable=False)
-    ip_address = db.Column(db.String(50), nullable=False, unique=True)
+    hostname = db.Column(db.String(64), nullable=False)
+    ip_address = db.Column(db.String(64), nullable=False)
     status = db.Column(db.String(20), default="RUNNING")
+    cpu_usage = db.Column(db.Float, default=0.0)
+    memory_usage = db.Column(db.Integer, default=0)
+    uptime = db.Column(db.Integer, default=0)
