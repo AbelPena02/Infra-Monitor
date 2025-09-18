a950bb56530df399650316f44b688ee0cb07374d
AbelPena02 <penafigueroaabel@gmail.com>
2025-09-17 21:14:45 -0700

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,4 @@ class Config:
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:" 
