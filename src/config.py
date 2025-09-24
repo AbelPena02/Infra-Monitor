80c18d572721a39befc0fb437fd194950d724c78
AbelPena02 <penafigueroaabel@gmail.com>
2025-09-23 20:25:19 -0700

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
+    DB_PORT = os.environ.get("DB_PORT", 5432)
+    DB_NAME = os.environ.get("DB_NAME", "infra_monitor")
+    DB_USER = os.environ.get("DB_USER", "monitor")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "monitor123")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+  
